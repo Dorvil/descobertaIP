659051198c81d59f94433bda260e8669351c46fc
Davidson Dorvil <86333511+Dorvil@users.noreply.github.com>
2023-11-19 05:01:56 -0300

    Update descobertaIP.py

--- descobertaIP.py
+++ descobertaIP.py
@@ -1,25 +1,39 @@
 import socket
 
-def obter_endereco_ip(nome_host):
+def obter_enderecos_ip(nome_host):
     try:
-        endereco_ip = socket.gethostbyname(nome_host)
-        return endereco_ip
+        enderecos_ip = socket.gethostbyname_ex(nome_host)[2]
+        return enderecos_ip
     except socket.gaierror as e:
         return str(e)
 
 def main():
-    print("Descobridor de Endereço IP por David")
+    nome_ferramenta = "DescobertaIP"
+    linha_estrelas = "*" * 67
+
+    print(linha_estrelas)
+    print("/*                           DESCRIÇÃO DA FERRAMENTA                          */")
+    print(f"*{'':<33}{nome_ferramenta.center(34)}{'':<33}*")
+    print(f"*{'':<33}{'Coded by Davidson Dorvil'.center(34)}{'':<33}*")
+    print(f"*{'':<33}{'GitHub: https://github.com/Dorvil'.center(34)}{'':<33}*")
+    print(linha_estrelas)
+
+    print("\nDescobridor de Endereços IP por David")
     print("------------------------------------")
 
     nome_host = input("Digite o nome do site (exemplo: www.exemplo.com): ")
 
-    endereco_ip = obter_endereco_ip(nome_host)
+    if not nome_host:
+        print("Nome de host inválido. Por favor, forneça um nome de host válido.")
+        return
+
+    enderecos_ip = obter_enderecos_ip(nome_host)
 
     print("\nResultado:")
     print("-----------")
-    
-    if endereco_ip:
-        print(f"O endereço IP associado ao site {nome_host} é: {endereco_ip}")
+
+    if enderecos_ip:
+        print(f"Os endereços IP associados ao site {nome_host} são: {', '.join(enderecos_ip)}")
     else:
         print(f"Não foi possível obter o endereço IP para o site {nome_host}.")
 
